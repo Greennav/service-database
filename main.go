13b45abc801e9681255cb8c17bea09b1c6d9c2d3
sitaramshelke <sitaramshelke@gmail.com>
2016-03-12 18:47:43 +0530

    Commented code for setting up postgres database, added new endpoint for vehicles

--- main.go
+++ main.go
@@ -5,27 +5,27 @@ import(
   "github.com/ant0ine/go-json-rest/rest"
 	"log"
 	"net/http"
-  "github.com/sitaramshelke/service-database/pbf_import"
+  _ "github.com/sitaramshelke/service-database/pbf_import"
 )
 
 func main(){
 	
-  dbhelper.SetupPGDatabase()		//setsup the database
+  // dbhelper.SetupPGDatabase()		//setup the database
 	
-	pbf_import.ImportFromHttp()		//reads from the pbf file and exports to the database
+	// pbf_import.ImportFromHttp()		//reads from the pbf file and exports to the database
 	
   api := rest.NewApi()
 	api.Use(rest.DefaultDevStack...)
 	router, err := rest.MakeRouter(
 		rest.Get("/nodes", dbhelper.GetAllNodeData),
     rest.Get("/node", dbhelper.GetNodeData),
-		rest.Get("/country",dbhelper.GetCountryName),
+		rest.Get("/greennav/vehicles",dbhelper.GetAllVehicles),
 		// rest.Post("/device",AddDevice),
 	)
 	if err != nil {
 		log.Fatal(err)
 	}
 	api.SetApp(router)
-	log.Fatal(http.ListenAndServe(":8888", api.MakeHandler()))
+	log.Fatal(http.ListenAndServe(":6833", api.MakeHandler()))
   
 }
